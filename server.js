99d1b60fd8aa5a308bda073941440148a040a60b
Deepak Kumar Verma <boostyourselfup@gmail.com>
2025-06-05 19:24:28 +0530

    Cors fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 // middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],

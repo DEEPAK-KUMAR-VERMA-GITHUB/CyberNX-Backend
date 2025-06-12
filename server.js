455ad34960e671edd04e52a640cc029d13abf9bc
Deepak Kumar Verma <167059921+DEEPAK-KUMAR-VERMA-GITHUB@users.noreply.github.com>
2025-06-12 21:05:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,24 +13,17 @@ await DbConnect();
 
 const app = express();
 
-const allowedOrigins = ['https://cyber-nx-frontend.vercel.app/', 'http://localhost:3000'];
 // middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: 'https://cyber-nx-frontend.vercel.app',
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 app.get("/",(req,res) => {
   res.send("Server is healthy");

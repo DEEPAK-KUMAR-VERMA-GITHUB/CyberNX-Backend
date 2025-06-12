0563a532060fcb86ad83e8307ee058b482aaf530
Deepak Kumar Verma <167059921+DEEPAK-KUMAR-VERMA-GITHUB@users.noreply.github.com>
2025-06-12 20:36:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 // middlewares
 app.use(
   cors({
-    origin: "*",
+    origin: ["https://cyber-nx-frontend.vercel.app/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
